e8d152e95362e3193ac2053ecced4608c16e02a3
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-04-22 17:18:09 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,6 @@
+const wrapper = document.querySelector(".wrapper"),
+generateBtn = wrapper.querySelector(".form"); 
 
+generateBtn.addEventListener("click", () => {
+    wrapper.classList.add("active"); 
+}); 
