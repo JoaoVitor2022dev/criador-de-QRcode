1536ff04c5605a2525914dc7cb33a27bca7c283f
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-05-06 01:50:59 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,12 +1,1 @@
-const InputValue = document.querySelector('#imputValue'); 
-const btnValue = document.querySelector('#btnValue'); 
-const imgQrCode = document.querySelector('#imgQrCode'); 
-const wrapper = document.querySelector('.wrapper'); 
-let  valueDefault; 
-
-//https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example to
-
-btnValue.addEventListener('click', () => {
-  let qrcodeValue = InputValue.value.trim();
-  if(!qrcodeValue || qrcodeValue === valueDefault ) return; 
-}) 
+const inputValue = document.querySelector('#input'); 
